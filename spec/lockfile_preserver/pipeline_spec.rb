3ce35e3aaf756dca9b3ed37e8827f6b7ae1b1245
JuanitoFatas <katehuang0320@gmail.com>
2016-05-24 16:43:53 +0800

    Add a pipeline to call multiple preservers

---
+++ spec/lockfile_preserver/pipeline_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe LockfilePreserver::Pipeline do
+  describe "#call" do
+    let(:original) { IO.read("spec/fixtures/Gemfile.lock") }
+    let(:updated) { IO.read("spec/fixtures/lockfiles/ruby_versions/Gemfile.lock") }
+
+    it "takes preservers" do
+      pipeline = described_class.new [
+        LockfilePreserver::BundledWith,
+        LockfilePreserver::RubyVersion,
+      ]
+
+      result = pipeline.call(original, updated)
+
+      expect(result).to eq original
+    end
+  end
+end
