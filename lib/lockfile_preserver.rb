d9789f908600d960f85c1b297463e01533579461
JuanitoFatas <katehuang0320@gmail.com>
2016-05-24 16:44:25 +0800

    Add keep ruby version ability and keep_all

--- lib/lockfile_preserver.rb
+++ lib/lockfile_preserver.rb
@@ -1,12 +1,25 @@
 require "lockfile_preserver/version"
+require "lockfile_preserver/pipeline"
 require "lockfile_preserver/bundled_with"
+require "lockfile_preserver/ruby_version"
 
 module LockfilePreserver
   def self.keep(original, updated, section = :bundled_with)
     if section == :bundled_with
       LockfilePreserver::BundledWith.new(original, updated).keep
-    else
-      abort "We currently only support preserve BUNDLED_WITH section of lockfile."
+    elsif section == :ruby_version
+      LockfilePreserver::RubyVersion.new(original, updated).keep
+    elsif
+      abort %(We currently only support preserve "BUNDLED WITH" & "RUBY VERSION" section of lockfile.)
     end
+  end
+
+  def self.keep_all(original, updated)
+    pipeline = Pipeline.new [
+      LockfilePreserver::BundledWith,
+      LockfilePreserver::RubyVersion  
+    ]
+
+    pipeline.call(original, updated)
   end
 end
--- spec/lockfile_preserver_spec.rb
+++ spec/lockfile_preserver_spec.rb
@@ -4,4 +4,42 @@ RSpec.describe LockfilePreserver do
   it "has a version number" do
     expect(LockfilePreserver::VERSION).not_to be nil
   end
+
+  let(:original) { IO.read("spec/fixtures/Gemfile.lock") }
+  let(:updated) { IO.read("spec/fixtures/lockfiles/ruby_versions/Gemfile.lock.rb24") }
+
+  describe ".keep" do
+    def exercise
+      described_class.keep(original, updated, section)
+    end
+
+    context "keep bundled with" do
+      let(:section) { :bundled_with }
+
+      it "invokes correct method" do
+        expect(LockfilePreserver::BundledWith).to receive(:new) { double(keep: true) }
+
+        exercise
+      end
+    end
+
+    context "keep ruby version" do
+      let(:section) { :ruby_version }
+
+      it "invokes correct method" do
+        expect(LockfilePreserver::RubyVersion).to receive(:new) { double(keep: true) }
+
+        exercise
+      end
+    end
+  end
+
+  describe ".keep_all" do
+    it "keep bundled with and ruby version" do
+      expect(LockfilePreserver::BundledWith).to receive(:new) { double(keep: true) }
+      expect(LockfilePreserver::RubyVersion).to receive(:new) { double(keep: true) }
+
+      described_class.keep_all(original, updated)
+    end
+  end  
 end
